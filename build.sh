1c1b187be89782969a4e326fe77f4c68c65e7911
Tommy Keswick <tkeswick@caltech.edu>
2017-04-28 09:43:42 -0700

    Update build script.

--- build.sh
+++ build.sh
@@ -3,21 +3,35 @@
 # This script is suitable to build the site after the correct directory
 # structure is set up, particularly within the `web/sites` directory.
 
-# Run it with `sudo` privileges.
+# Run this script with `sudo` privileges.
 
-# Get the directory of this script.
-project_root=`dirname $0`
+# Map directories and files.
+script_root=$(dirname "$0")
+cd "$script_root" || exit
+project_root=$(pwd)
+cd "$project_root" || exit
+settings_file=$(find "$project_root" -type f | grep -E "\/settings\.php$")
+site_root=$(dirname "$settings_file")
 
-# Change directory to web root.
-cd ${project_root}/web
+# Find all `.git` directories and pull new code for those repositories.
+find "$project_root" -type d | grep -E "\.git$" | while read -r repo; do
+  cd "$repo" || exit
+  cd .. || exit
+  echo "$PWD"
+  git pull
+done
 
-# Update Drupal codebase via Git.
-git pull
+# Check for `htaccess.patch` file.
+if find "$site_root" -type f | grep -E "\/htaccess\.patch$"
+  then
+    # Check if `.htaccess` customizations are present.
+    if ! grep -Fxq "# BEGIN CUSTOMIZATIONS" "$project_root"/web/.htaccess
+      then
+        # Apply patch to `.htaccess` file to add customizations.
+        git apply -v "$site_root"/patches/htaccess.patch
+    fi
+fi
 
-# get list of custom module directories
-# loop through list and git pull inside each one
-
-# get list of custom theme directories
-# loop through list and git pull inside each one
-
-# run drush updb
+# Update the Drupal database.
+cd "$site_root" || exit
+drush updb -y
